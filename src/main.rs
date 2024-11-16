8bd55e3a976965a2e21a51ee25912516472a3577
AirKoala <airkoala1@gmail.com>
2024-11-16 20:44:11 +1100

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use dotenv::dotenv;
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+struct MainHandler;
+
+#[async_trait]
+impl EventHandler for MainHandler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN env var not set.");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(MainHandler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
+}
